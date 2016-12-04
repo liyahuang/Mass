9f9432e2261c49c1e3826966b532dddead507006
Johnny4Fun <309329794@qq.com>
2016-12-05 06:52:13 +0800

    correct module01.c

--- job2/module01.c
+++ job2/module01.c
@@ -1,11 +1,11 @@
 #include<linux/init.h>
 #include<linux/module.h>
 #include<linux/kernel.h>
-
+#include<linux/sched.h>
 static int display_init(void)
 {
-	struct task_struct *p;
-	p = NULL;
+	struct task_struct *p=NULL;
+
 	for_each_process(p)
 	{
 		printk(KERN_ALERT"%20s%20d%10d",p->comm,p->pid,p->state);

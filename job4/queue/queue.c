45e84f3e3c8022148aa97bb727e443b65e7cd4f4
Johnny4Fun <309329794@qq.com>
2016-12-08 22:09:19 +0000

    demo

--- job4/queue/queue.c
+++ job4/queue/queue.c
@@ -6,10 +6,20 @@
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/stat.h>
+#include <pthread.h>
+#include <semaphore.h>
 #define MSG_FILE "queue.c"
 #define MSG_NUM 1024
 #define BUFFER 255
+#define SENDER_FLAG 1
+#define RECEIVER_FLAG 2
 #define PERM S_IRUSR|S_IWUSR
+sem_t sem;
+if(-1 == sem_init(&sem,0,1))
+{
+	fprintf(stderr,"semaphore init error: %s\n",strerror(errno));
+	exit(EXIT_FAILURE);
+}
 struct msgbuf
 {
 	long mtype;
@@ -19,7 +29,7 @@ void send()
 {
 	struct msgbuf msg;
 	key_t key;
-	int msgid;
+	int msqid;
 	char *myask = "I'm receiver";
 	int ret_value;
 	char message[BUFFER+1];
@@ -34,26 +44,56 @@ void send()
 		fprintf(stderr,"creat msg queue error:%s\n",strerror(errno));
 		exit(EXIT_FAILURE);
 	}
-	msg.mtype = 1; 
-	while(1)
+	msg.mtype = SENDER_FLAG; 
+	do
 	{
-		scanf("%s",s_buf);
-		strcpy(msg.mtext,message);
-		if(strcmp(s_buf,"exit");
+		scanf("%s",message);
+		strncpy(msg.mtext,message,BUFFER);
+		if(-1 == msgsnd(msqid,&msg,sizeof(struct msgbuf)-sizeof(long),0))
 		{
-			strcpy(msg.mtext,"end");
-			msgsnd(msgid,&msg,sizeof(struct msgbuf),0);
-			//发送内容为"end"的消息
-			break;
+			fprintf(stderr,"msg send error: %s",strerror(errno));
 		}
-		msgsnd(msgid,&msg,sizeof(struct msgbuf),0);
-		//发送消息
 	}
-
+	while(strcmp(message,"exit") != 0);
+	strcpy(msg.mtext,"end");
+	msgsnd(msqid,&msg,sizeof(struct msgbuf)-sizeof(long),0);//send "end" message
+	
+	//接收
+	do
+	{
+		memset(&msg,'\0',sizeof(struct msgbuf));
+		msgrcv(msqid,&msg,sizeof(struct msgbuf)-sizeof(long),RECEIVER_FLAG,0);
+	}while( 0 != strcmp(msg.mtext,"over"));
+	//结束程序运行
+	//exit(0);
+	
 }
 void receive()
 {
-	
+	key_t key;
+	int msqid;
+	msgbuf msg;
+	if (-1 == (key = ftok(MSG_FILE,MSG_NUM)))
+	{
+		fprintf(stderr,"creat key error:%s\n",strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+    if(-1 == (msqid=msgget(key,PERM|IPC_CREAT)))
+    {
+        fprintf(stderr,"Creat MessageQueue Error：%s \n",strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+	do
+	{
+		msgrcv(msqid,&msg,BUFFER+1,SENDER_FLAG,0);
+		if()
+		{
+			
+		}
+	}while( 0 != strcmp(msg.mtext,"end"));
+	msg.mtype = RECEIVER_FLAG;
+	strcpy(msg.mtext,"over");
+	msgsnd(msqid,&msg,BUFFER+1,0);
 }
 int main()
 {
